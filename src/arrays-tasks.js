9794b8a851eed8d52063c404d0f0876afd11c37d
Anna Vasilevich <dzichonka@gmail.com>
2024-12-08 21:22:31 +0100

    feat: done 47/49

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -547,8 +547,11 @@ function findLongestIncreasingSubsequence(/* nums */) {
  *  propagateItemsByPositionIndex([ 'a', 'b', 'c', null ]) => [ 'a', 'b', 'b', 'c', 'c', 'c',  null, null, null, null ]
  *  propagateItemsByPositionIndex([ 1,2,3,4,5 ]) => [ 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  const res = arr.flatMap((item, i) => {
+    return new Array(i + 1).fill(item);
+  });
+  return res;
 }
 
 /**
@@ -564,8 +567,12 @@ function propagateItemsByPositionIndex(/* arr */) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  const newArr = arr;
+  if (n > 0) {
+    return newArr.splice(-n, n).concat(...newArr);
+  }
+  return newArr.concat(...newArr.splice(0, -n));
 }
 
 /**
@@ -604,8 +611,12 @@ function sortDigitNamesByNumericOrder(/* arr */) {
  *   swapHeadAndTail([]) => []
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const newArr = arr;
+  const lenOfHalf = Math.floor(arr.length / 2);
+  const head = newArr.splice(0, lenOfHalf);
+  const tail = newArr.splice(-lenOfHalf, lenOfHalf);
+  return tail.concat(...newArr, ...head);
 }
 
 module.exports = {
